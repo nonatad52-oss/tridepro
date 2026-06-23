b24e745eb36661ee473c0eb419efdc56c05a1bb9
nonatad52-oss <nonatad52@gmail.com>
2026-06-23 08:01:50 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['yahoo-finance2'],
+  experimental: {
+    serverComponentsExternalPackages: ['yahoo-finance2'],
+  },
 };
 
 export default nextConfig;
